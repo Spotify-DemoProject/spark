e17b149b353816a6c1ab29c71672416dfbc76e54
hooniegit <neivekim76@gmail.com>
2023-12-30 17:13:30 +0900

    231230 feat & test: dataframe 가공 로직 수정 및 동작 테스트

--- lib/create_dataframe.py
+++ lib/create_dataframe.py
@@ -3,12 +3,13 @@ def create_artists(spark, json_dir:str):
     from pyspark.sql.functions import col, explode, expr
     
     df = spark.read.option("multiline", "true").json(json_dir) \
-    .withColumn("image", explode(expr("images"))) \
+    .select(explode('artists').alias('artists')) \
+    .withColumn("image", explode(expr("artists.images"))) \
     .filter(col("image.height") == 640) \
-    .withColumn("artist_id", expr("id")) \
-    .withColumn("artist_name", expr("name")) \
-    .withColumn("genre", explode(expr("genres"))) \
-    .withColumn("followers", expr("followers.total")) \
+    .withColumn("artist_id", expr("artists.id")) \
+    .withColumn("artist_name", expr("artists.name")) \
+    .withColumn("genre", explode(expr("artists.genres"))) \
+    .withColumn("followers", expr("artists.followers.total")) \
     .withColumn("artist_image_url", expr("image.url")) \
     .select("artist_id", "artist_name", "genre", "artist_image_url", "followers")
     
@@ -18,27 +19,22 @@ def create_albums(spark, json_dir:str):
     from pyspark.sql.functions import col, explode, expr
     
     df = spark.read.option("multiline", "true").json(json_dir) \
-    .withColumn("image", explode(expr("images"))) \
-    .filter(col("image.height") == 640) \
-    .withColumn("artist_id", explode(expr("artists.id"))) \
-    .withColumn("album_id", expr("id")) \
-    .withColumn("album_name", expr("name")) \
-    .withColumn("album_image_url", expr("image.url")) \
-    .withColumn("track", explode(expr("tracks.items"))) \
-    .withColumn("track_id", expr("track.id")) \
-    .withColumn("disc_number", expr("track.disc_number")) \
-    .withColumn("track_number", expr("track.track_number")) \
-    .select("album_id", "album_name", "artist_id", "album_type", "label", "album_image_url", "track_id", "track_number", "release_date", "total_tracks")
+        .select(explode('albums').alias('albums')) \
+        .withColumn("image", explode(expr("albums.images"))) \
+        .filter(col("image.height") == 640) \
+        .withColumn("artist_id", explode(expr("albums.artists.id"))) \
+        .withColumn("album_id", expr("albums.id")) \
+        .withColumn("album_name", expr("albums.name")) \
+        .withColumn("album_image_url", expr("image.url")) \
+        .withColumn("track", explode(expr("albums.tracks.items"))) \
+        .withColumn("track_id", expr("track.id")) \
+        .withColumn("track_name", expr("track.name")) \
+        .withColumn("duration_ms", expr("track.duration_ms")) \
+        .withColumn("explicit", expr("track.explicit")) \
+        .withColumn("disc_number", expr("track.disc_number")) \
+        .withColumn("track_number", expr("track.track_number")) \
+        .select("album_id", "album_name", "artist_id", "albums.album_type", "albums.label", "album_image_url", "track_id", "track_name", "track_number", "disc_number", "duration_ms", "explicit", "albums.release_date", "albums.total_tracks")
+
     
     return df
 
-def create_tracks(spark, json_dir:str):
-    from pyspark.sql.functions import explode, expr
-    
-    df = spark.read.option("multiline", "true").json(json_dir) \
-    .withColumn("track_id", expr("id")) \
-    .withColumn("track_name", expr("name")) \
-    .withColumn("artist_id", explode(expr("artists.id"))) \
-    .select("track_id", "track_name", "artist_id", "duration_ms", "explicit")
-    
-    return df
